25902ee33088cd307d3fe95249c52eecb7bda163
Damien Smrt <damien@flipboxdigital.com>
2018-07-31 11:25:17 -0600

    adding change log and fixing colors when the system doesn't support it.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+## [1.0.1] 2018-07-31
+### Fixed
+- fixed colored output so it doesn't error of the $TERM variable is empty
+
+## [1.0.0] 2018-07-28
+### Added
+- Initial release
--- bin/.text-decoration.sh
+++ bin/.text-decoration.sh
@@ -7,15 +7,17 @@
 #6    cyan      COLOR_CYAN      0,1,1
 #7    white     COLOR_WHITE     1,1,1
 
-FG_BLACK=`tput setaf 0`
-FG_RED=`tput setaf 1`
-FG_GREEN=`tput setaf 2`
-FG_YELLOW=`tput setaf 3`
-FG_BLUE=`tput setaf 4`
-FG_MAGENTA=`tput setaf 5`
-FG_CYAN=`tput setaf 6`
-FG_WHITE=`tput setaf 7`
+if [ -n "$TERM" ]; then
+    FG_BLACK=`tput setaf 0`
+    FG_RED=`tput setaf 1`
+    FG_GREEN=`tput setaf 2`
+    FG_YELLOW=`tput setaf 3`
+    FG_BLUE=`tput setaf 4`
+    FG_MAGENTA=`tput setaf 5`
+    FG_CYAN=`tput setaf 6`
+    FG_WHITE=`tput setaf 7`
 
-RESET=`tput sgr0`
-BOLD=`tput bold`
+    RESET=`tput sgr0`
+    BOLD=`tput bold`
+fi
 
